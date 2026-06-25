e34e655712b2cd64cf64544f17de62ea06f200f5
Prisca Nwobu <nwobukenechukwu14@gmail.com>
2026-06-25 10:21:36 +0100

    Finished-ish

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,25 @@
-import Search from "./Search";``
+import Background from './Background.jpg'
+import { IoSearch } from "react-icons/io5";
+import { Link } from 'react-router-dom';
 
 export default function Home(){
 
     return(
-        <div>
-            <Search />
+        <div style={{
+            backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${Background})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            height: "100vh", 
+        }} className=''>
+
+            <div className="flex items-center p-10 h-full font-bold text-4xl w-150 text-[#F4C430]">
+                <p className=''>Come with us to discover your favourite pokemons</p>
+                <p>
+                    <Link to='/pokemons'>
+                    <IoSearch />
+                    </Link>
+                </p>
+            </div>
         </div>
     )
 }
--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -20,30 +20,32 @@ export default function PokemonList(){
 
     return(
         <div>
-            <div className="">
+            <div className=" flex flex-col gap-3">
 
-                <Search />
-
-                <div className="">
+                <div className="text-2xl">
                     <p>20 of your favourite pokemons</p>
+                </div>
+
+                <div className="flex justify-center w-full">
+                    <Search />
                 </div>
 
                 <div className="">
                     <Link to='/'>
-                    <button> <FaArrowLeft /> Home</button>
+                    <button className="flex items-center gap-4 p-2 border-2 border-[#F4C430] rounded-2xl text-base hover:bg-[#F4C430] hover:text-[#060B19]"> <FaArrowLeft /> Home</button>
                     </Link>
-
-                    <div className="list">
-                        {
-                            filteredPokemon.map((poke, index) => (
-                                <PokemonCard
-                                key={index}
-                                PokeName={poke.name}
-                                />
-                           ))
-                        }
-                    </div>
                 </div>
+                <div className="text-xl flex flex-col gap-2 p-1 border-b-2 border-[#F4C430]">
+                    {
+                        filteredPokemon.map((poke, index) => (
+                            <PokemonCard
+                            key={index}
+                            PokeName={poke.name}
+                            />
+                        ))
+                    }
+                </div>
+                
             </div>
         </div>
     )
